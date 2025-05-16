8b6d9ccf760b1c3a03d07eea3c17757b4eab7d6e
chirag-patel-499 <cp0824279@gmail.com>
2025-05-16 15:11:56 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,28 @@
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from .forms import ContactForm
+
+# HomeView for homepage
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+# AboutView for about page
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+# ContactView with form
+class ContactView(FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('contact')
+    def form_valid(self, form):
+        # You can save data or send email here
+        form.save()  # Only if your form is linked to a model
+        return super().form_valid(form)
+    
+class VideoView(TemplateView):
+    template_name = 'videos-1.html'
+
+class WorkView(TemplateView):
+    template_name = 'work.html'        
---
+++ sareh_dj/urls.py
@@ -0,0 +1,23 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from main.views import HomeView, AboutView, ContactView, VideoView, WorkView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'), 
+    path('about/', AboutView.as_view(), name='about'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('videos-1/', VideoView.as_view(), name='videos-1'),
+    path('videos-2/', VideoView.as_view(), name='videos-2'),
+    path('videos-3/', VideoView.as_view(), name='videos-3'),
+    path('video-details/', VideoView.as_view(), name='video-details'),
+    path('work/', WorkView.as_view(), name='work')
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
