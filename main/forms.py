2f71988ec80256eb4ed462cb8f46ced6be5eeb7e
chirag-patel-499 <cp0824279@gmail.com>
2025-07-01 14:12:52 +0530

    contect and forms edit

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,21 @@
 from django import forms
-from .models import Comment
-
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+from .models import Comment, Contact
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'comment']    
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'phone', 'subject', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Full Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email Address'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Phone Number'}),
+            'subject': forms.TextInput(attrs={'placeholder': 'Subject'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Messages'}),
+        }
