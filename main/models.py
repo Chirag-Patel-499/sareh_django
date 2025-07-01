9b87901f0ecad7a33b3a119a804587f1297e1097
chirag-patel-499 <cp0824279@gmail.com>
2025-07-01 14:05:07 +0530

    models.py added

--- main/models.py
+++ main/models.py
@@ -20,6 +20,20 @@ class Hero(models.Model):
     
 
 
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
+    
+    
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, blank=True)
